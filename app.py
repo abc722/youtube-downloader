c399010cecb167cbd2223b416a204135bf855e85
Fatih Boz <93795733+abc722@users.noreply.github.com>
2023-01-08 15:58:30 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+main code 
+
+import youtube_indir as downloader
+
+link = input("Link")
+v = downloader.Video(link) 
+v.download()
+
+v2 = downloader.Video("https://youtu.be/pJRzKOqxQJ0")
+v2.description()
